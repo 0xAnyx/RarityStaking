1b3730e6391512d93065deffd89aea673be5ac57
Anik Ghosh <itsmenick30@gmail.com>
2022-05-04 16:10:10 +0530

    updated

--- test/Rarity-Test.js
+++ test/Rarity-Test.js
@@ -5,12 +5,12 @@ const { advanceTime } = require('./utils');
 const Web3 = require("web3");
 const { fromWei } = Web3.utils;
 
-describe('Rarity Staking Test Suite', async function(){
-    let owner, alice, bob, nft, token, staking;
+describe('///^Rarity Staking Test Suite^///', async function(){
+    let owner, alice, bob, clyde, nft, token, staking;
 
     beforeEach('Running Iterative Functions', async function(){
 
-        [owner, alice, bob] = await ethers.getSigners();
+        [owner, alice, bob, clyde] = await ethers.getSigners();
         const nftToken = await ethers.getContractFactory('MockNFT');
         nft = await nftToken.deploy();
         const rewardToken = await ethers.getContractFactory('MockToken');
@@ -22,9 +22,12 @@ describe('Rarity Staking Test Suite', async function(){
         await nft.connect(alice).safeMint(alice.address, 2);
         await nft.connect(bob).safeMint(bob.address, 3);
         await nft.connect(bob).safeMint(bob.address, 4);
+        await nft.connect(clyde).safeMint(clyde.address, 1466);
+        await nft.connect(clyde).safeMint(clyde.address, 565);
 
         await nft.connect(alice).setApprovalForAll(staking.address, true);
         await nft.connect(bob).setApprovalForAll(staking.address, true);
+        await nft.connect(clyde).setApprovalForAll(staking.address, true);
 
         await token.mint(owner.address, ethers.utils.parseEther('6000'));
         await token.transfer(staking.address, ethers.utils.parseEther('5000'));
@@ -61,7 +64,7 @@ describe('Rarity Staking Test Suite', async function(){
             fromWei(balanceOfReward1.toString(),'ether'));
         await staking.connect(alice).raffleRoll([1,2]);
         let balanceOfReward2 = await token.balanceOf(alice.address);
-        console.log("Alice's balance before winning Raffle: -",
+        console.log("Alice's balance after winning Raffle: -",
             fromWei(balanceOfReward2.toString(),'ether'));
         await advanceTime(3600 * 11);
         await expect(staking.connect(alice).raffleRoll([1, 2])).to.be.revertedWith('Rolling too soon');
@@ -92,21 +95,21 @@ describe('Rarity Staking Test Suite', async function(){
         console.log("Alice can't claim for Bob");
         await staking.connect(bob).claimRewards([4]);
         let balanceOfReward2 = await token.balanceOf(bob.address);
-        console.log("Bob's balance after claiming:-",
+        console.log("Bob's balance after claiming only for tokenId 4:-",
             fromWei(balanceOfReward2.toString(),'ether'));
         await staking.connect(bob).claimRewards([4]);
         let balanceOfReward3 = await token.balanceOf(bob.address);
-        console.log("Bob's balance after claiming right after:-",
+        console.log("Bob's balance after claiming reward for tokenId 4 immediately:-",
             fromWei(balanceOfReward3.toString(),'ether'));
         await staking.updateRewards(ethers.utils.parseEther('2'));
         await staking.connect(bob).claimRewards([3]);
         let balanceOfReward4 = await token.balanceOf(bob.address);
-        console.log("Bob's balance after claiming for 3 after changing rate but immediately:-",
+        console.log("Bob's balance after claiming for tokenId 3 after changing rate but immediately:-",
             fromWei(balanceOfReward4.toString(),'ether'));
         await advanceTime(12 * 3600);
         await staking.connect(bob).claimRewards([3]);
         let balanceOfReward5 = await token.balanceOf(bob.address);
-        console.log("Bob's balance after claiming for 3 after changing rate:-",
+        console.log("Bob's balance after claiming for tokenID 3 after changing rate after 12 hrs:-",
             fromWei(balanceOfReward5.toString(),'ether'));
         await advanceTime(12 * 3600);
     });
@@ -130,9 +133,32 @@ describe('Rarity Staking Test Suite', async function(){
             fromWei(balanceOfReward2.toString(),'ether'));
         expect (await nft.ownerOf(3)).to.equal(bob.address);
         console.log("NFTs returned to original owners");
-        console.log(await staking.stakedInfo(3));
         let structVar2 = await staking.stakedInfo(3);
         console.log("Struct owner value:-",structVar2[0]);
     });
 
+    it('Test 5: Testing for the rarest and least rare Molecule', async function(){
+        await staking.initializeRarity([[1466, 2859033,
+        "0x4918ee4734b133c3e0badabc708dec50467480950e67249eb130d9f1794d57ef74d049e9d3eec1e1f994ea8c31de99596a4c234436f98e961124da2a51c378b91b"],
+        [565, 277489,
+        "0x4a53de024665ce4464c6d5dfb7bdb058f3e74a3a5db9c1cab0e3b88fee73ff3b7bd83be6c137f542546a017e043c113afc100deac0650a2c01c58102d551b9941c"]]);
+        await staking.connect(clyde).stakeTokens([1466, 565]);
+        await advanceTime(24 * 3600);
+
+        let balanceOfReward1 = await token.balanceOf(clyde.address);
+        console.log("Clyde's balance before claiming Reward for rarest molecule: -",
+            fromWei(balanceOfReward1.toString(),'ether'));
+        await staking.connect(clyde).claimRewards([1466]);
+        let balanceOfReward2 = await token.balanceOf(clyde.address);
+        console.log("Clyde's balance after claiming Reward for tokenID 1466(rarest) after 24hrs: -",
+            fromWei(balanceOfReward2.toString(),'ether'));
+
+        let balanceOfReward3 = await token.balanceOf(clyde.address);
+        console.log("Clyde's balance before claiming reward for least rare molecule: -",
+            fromWei(balanceOfReward3.toString(),'ether'));
+        await staking.connect(clyde).claimRewards([565]);
+        let balanceOfReward4 = await token.balanceOf(clyde.address);
+        console.log("Clyde's balance after claiming reward for tokenID 565(least rare) after 24hrs: -",
+            fromWei(balanceOfReward4.toString(),'ether'));
+    });
 });
